b1959ce394c41608b18fc7e2379ac06e3ba4efb2
MunisaR <91838888+MunisaR@users.noreply.github.com>
2021-12-06 15:35:34 +0500

    webtech

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//  nav bar toggling
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+// validation form to protect the site
+const first = document.getElementById('first')
+const second = document.getElementById('second')
+const btn = document.getElementById('calculate')
+const result = document.getElementById('result')
+const  user = document.getElementById('user')
+
+first.innerHTML = Math.floor(Math.random()*10)
+second.innerHTML = Math.floor(Math.random()*10)
+
+
+
+
+const test =  document.getElementById('test')
+btn.addEventListener('click', ()=>{
+    correctAnswer = parseInt(first.innerHTML) + parseInt(second.innerHTML)
+    
+    if (parseInt(user.value) == correctAnswer){
+        result.innerHTML = 'now you can get an appointment'
+        test.classList.add('activeee')
+    }
+    else{
+        result.innerHTML = ' try again '
+    }
+    
+})
